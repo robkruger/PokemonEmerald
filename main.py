1361353e117e804647959a1e57b4263b823c6fe0
Rob Krüger <4958@praedinius.nl>
2019-12-31 12:21:22 +0100

    some screenshots

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 from game.game import Game
 from mapmaker.map_maker import MapMaker
 
-# m = MapMaker((600, 400), (10, 15), (1408, 0))
+m = MapMaker((600, 400), (10, 15), (1408, 0))
+
+while m.Running:
+    m.parse_events()
+    m.draw()
+
+m.close()
+
+# g = Game('data.npy', (600, 400))
 #
-# while m.Running:
-#     m.parse_events()
-#     m.draw()
+# while g.Running:
+#     g.parse_events()
+#     g.draw()
 #
-# m.close()
-
-g = Game('data.npy', (600, 400))
-
-while g.Running:
-    g.parse_events()
-    g.draw()
-
-g.close()
+# g.close()
 
